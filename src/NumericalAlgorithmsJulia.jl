177fd1169511ea649813e89ecfe7116604362de2
Murat Koptur <mkoptur3@gmail.com>
2021-01-23 21:54:15 +0300

    init

---
+++ src/NumericalAlgorithmsJulia.jl
@@ -0,0 +1,5 @@
+module NumericalAlgorithmsJulia
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,7 @@
+using Test
+
+@testset "Foo Tests" begin
+    @test 1 == 1
+    @test 4 == 4
+    @test 9 == 9
+end;
