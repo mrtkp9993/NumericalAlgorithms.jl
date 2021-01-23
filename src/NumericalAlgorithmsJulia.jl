b0b32da52c0d9987cc55f1b9c84fd3a70f9500ac
Murat Koptur <mkoptur3@gmail.com>
2021-01-23 22:37:07 +0300

    rootfinding1d added

--- src/NumericalAlgorithmsJulia.jl
+++ src/NumericalAlgorithmsJulia.jl
@@ -1,5 +1,19 @@
 module NumericalAlgorithmsJulia
 
-greet() = print("Hello World!")
+function FindRoot1D(f::Function, p0::Float64, p1::Float64, N0::Int64, tol::Float64)
+    i = 1
+    while i <= N0
+        p2 = p0 - f(p0) * ((p1 - p0) / (f(p1) - f(p0)))
+        if abs(p2 - p1) < tol
+            println("Root found after $i iterations, root: $p2")
+            return p2
+        end
+        i += 1
+        p0 = p1
+        p1 = p2
+    end
+    println("Method failed after $i iterations.")
+    return NaN
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,12 @@
+using NumericalAlgorithmsJulia
 using Test
 
-@testset "Foo Tests" begin
-    @test 1 == 1
-    @test 4 == 4
-    @test 9 == 9
+@testset "Root finding" begin
+    f1(x::Float64)::Float64 = x * x - x - 1
+    p0 = 1.5
+    p1 = 2.0
+    tol = 1e-5
+    N0 = 100
+    res = NumericalAlgorithmsJulia.FindRoot1D(f1, p0, p1, N0, tol)
+    @test round(res, digits=5) == 1.61803
 end;
