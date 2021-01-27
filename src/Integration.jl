b7d9434e8e23ed539ca580db188b233ca5e8da33
Murat Koptur <mkoptur3@gmail.com>
2021-01-27 16:23:51 +0300

    Update Integration.jl

--- src/Integration.jl
+++ src/Integration.jl
@@ -1,15 +1,15 @@
 """
-    CalcSingleIntegral(f::Function, a::Real, b::Real, NMAX::UInt64)
+    CalcSingleIntegral(f::Function, a::Real, b::Real, n::UInt64)
 
-Compute integral of f function from a to b.
+Compute integral of f function from a to b (Composite Simpson method).
 """
-function CalcSingleIntegral(f::Function, a::Real, b::Real, NMAX::UInt64)
+function CalcSingleIntegral(f::Function, a::Real, b::Real, n::UInt64)
     xi = 0.0
-    h = (b - a) / NMAX
+    h = (b - a) / n
     xi0 = f(a) + f(b)
     xi1 = 0.0
     xi2 = 0.0
-    for i = 1:NMAX
+    for i = 1:(n - 1)
         x = a + i * h;
         if mod(i, 2) == 0
             xi2 = xi2 + f(x)
@@ -20,4 +20,14 @@ function CalcSingleIntegral(f::Function, a::Real, b::Real, NMAX::UInt64)
     xi = h * (xi0 + 2 * xi2 + 4 * xi1) / 3;
     println("Approximation to integral: $xi")
     return xi
+end
+
+"""
+    CalcDoubleIntegral(f::Function, a::Real, b::Real, c::Real, d::Real, NMAX::UInt64)
+
+Compute double integral of f function over domain D=[a, b] x [c, d]
+(Double Simpson's method).
+"""
+function CalcDoubleIntegral(f::Function, a::Real, b::Real, c::Real, d::Real, NMAX::UInt64)
+    
 end
